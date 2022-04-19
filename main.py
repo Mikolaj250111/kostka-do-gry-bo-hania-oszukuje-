d036f611daf2d0a2d8581073f83c712449fd3ae1
Mikolaj250111 <95536365+Mikolaj250111@users.noreply.github.com>
2022-04-19 10:09:02 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    if (randint(1, 6) == 0) {
+    	
+    }
+})
+basic.forever(function () {
+	
+})
---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(randint(1, 6))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(randint(1, 6))
+})
+basic.showNumber(0)
+basic.forever(function () {
+	
+})
