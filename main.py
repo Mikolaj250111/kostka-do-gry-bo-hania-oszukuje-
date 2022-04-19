2dee6e2bf58563e0c6236ce55f72bb66004199f8
Mikolaj250111 <95536365+Mikolaj250111@users.noreply.github.com>
2022-04-19 10:49:39 +0200

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-input.onButtonPressed(Button.A, function () {
-    if (randint(1, 6) == 0) {
-    	
-    }
-})
-basic.forever(function () {
-	
-})
+def on_button_pressed_a():
+    basic.show_number(randint(1, 6))
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    basic.show_number(0 + 0)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    basic.show_number(randint(1, 6))
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+basic.show_number(0)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,10 +1,15 @@
+let cyfra_a = 0
+let cyfra_c = 0
+let cyfra_b = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(randint(1, 6))
+    basic.showNumber(cyfra_a)
+    cyfra_a = randint(1, 6)
+})
+input.onButtonPressed(Button.AB, function () {
+    cyfra_c = cyfra_a + cyfra_b
+    basic.showNumber(cyfra_c)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(randint(1, 6))
-})
-basic.showNumber(0)
-basic.forever(function () {
-	
+    basic.showNumber(cyfra_b)
+    cyfra_b = randint(1, 6)
 })
